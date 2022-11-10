9b6bab1aae8ea0a0d2f347e73b88bb694090db74
unknown <khoalkdgcs200265@fpt.edu.vn>
2022-11-10 14:33:26 +0700

    first commit

---
+++ Flight Doc System/Models/IdentityModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+
+namespace Flight_Doc_System.Models
+{
+    public class ApplicationUser : IdentityUser   
+    {
+        [Display(Name = "Full Name"),Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        public bool Gender { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplcationCookie);
+            return userIdentity;
+        }
+
+        public class Application : IdentityRole
+        {
+            public ApplicationRole() : base() { }
+            public ApplicationRole() : base(rolename) { }
+
+        }
+
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public ApplicationDbContext() : base("DefaultConnection", throw: false) { }
+        }
+    }
+}
