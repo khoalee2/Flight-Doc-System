667da593309925e82378ba610315a684d67d6699
unknown <khoalkdgcs200265@fpt.edu.vn>
2022-11-17 13:45:58 +0700

    Add existing file

--- Flight Doc System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Flight_Doc_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Flight_Doc_System/Migrations/202211150431549_document.cs
@@ -0,0 +1,30 @@
+﻿namespace Flight_Doc_System.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class document1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Documents",
+                c => new
+                    {
+                        DocumentName = c.String(nullable: false, maxLength: 128),
+                        Type = c.String(),
+                        FlightNo = c.String(),
+                        DepartureDate = c.DateTime(nullable: false),
+                        Creator = c.String(),
+                        UpdatedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.DocumentName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Documents");
+        }
+    }
+}
